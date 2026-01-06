9d33b9c9896622b36ee2d212c059d636feb8dff8
musicmaker-web <musicmaker.tiktok1@gmail.com>
2026-01-06 17:31:53 +0100

    Update script loading with encryption
    Refactored script loading mechanism to use encryption and decryption.

--- Build And Survive Script.lua
+++ Build And Survive Script.lua
@@ -1,11 +1,11 @@
-local HttpService = game:GetService("HttpService")
+local KEY = 73
 local url = "https://raw.githubusercontent.com/musicmaker-web/Roblox-Scripts/refs/heads/main/Build%20And%20Survive.lua"
+local encrypted = loadstring(game:HttpGet(url))()
 
-local success, err = pcall(function()
-    local scriptText = game:HttpGet(url)
-    loadstring(scriptText)() 
-end)
-
-if not success then
-    warn("Error loading the Script: "..err)
+local decrypted = {}
+for i = 1, #encrypted do
+    decrypted[i] = string.char(bit32.bxor(encrypted:byte(i), KEY))
 end
+
+local source = table.concat(decrypted)
+loadstring(source)()
